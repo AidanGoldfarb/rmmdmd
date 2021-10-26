da47213634c4270883bbbd9879fc9c211158ea2b
Aidan <agoldfa7@u.rochester.edu>
2021-10-26 18:06:21 -0400

    atn

--- src/a.rs
+++ src/a.rs
@@ -1,4 +1,5 @@
-pub fn a_t_n(i:usize, j:usize, n: usize) -> f64{
+#[allow(unused)]
+pub fn atn(i:usize, j:usize, n: usize) -> f64{
     let mut i = i as f64;
     let mut j = j as f64;
     let n = n as f64;
@@ -20,4 +21,30 @@ pub fn a_t_n(i:usize, j:usize, n: usize) -> f64{
         n_temp /= 2.0;
     }
     return t;
+}
+#[allow(unused)]
+pub fn my_atn(n: usize) -> f64{
+    let n = n as f64;
+    
+    let t1 = t(n);
+    let t2 = 2.0*n*n;
+    let t3 = 8.0*t(n/2.0);
+    let mut t4 = 0.0;
+    let mut count = n;
+    let mut i = 1.0;
+    while count > 1.0 {
+        t4 += 2.0*t(2.0_f64.powf(i-1.0));
+        count /= 2.0;
+        i += 1.0;
+    }
+    return t1 + t2 + t3 - t4;
+}
+
+#[allow(unused)]
+fn t(n: f64) -> f64{
+    return n*n*(2.0*n-1.0);
+}
+#[allow(unused)]
+fn d(n: f64) -> f64{
+    return t(n) + 2.0*n*n;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod a;
-use a::{a_t_n};
+use a::{my_atn};
 
 fn main() {
-    let t = a_t_n(1,2,15);
+    let (_a,_b,c) = (1,2,8);
+    let  t = my_atn(c);
     println!("{}", t);
+    // t = my_atn(a,b,c);
+    // println!("{}", t);
 }
